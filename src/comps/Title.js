a8afa47a88ef36e5dd9fbcfab7bf49be58f99eea
ritikaawasthy <ritika.awasthy@gmail.com>
2021-04-24 22:26:43 +0530

    changed content

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>Catogram</h1>
+      <h2>Catogram</h2>
+      <p>Welcome! This is instagram for cats, upload your kitty here. MEOW</p>
     </div>
   )
 }
